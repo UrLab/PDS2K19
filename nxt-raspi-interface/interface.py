37ae4595194e07d94215c6205c5c90a9414eb410
Herrgrim0 <rgrimau@ulb.ac.be>
2019-03-19 20:36:27 +0100

    [add] basic movement functons

---
+++ nxt-raspi-interface/interface.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import nxt.locator
+from time import sleep
+from nxt.motor import *
+
+FORTH = 100
+BACK = -100
+
+def left(b):
+    m_left = Motor(b, PORT_B)
+    m_left.turn(100, 15)
+
+def right(b):
+    m_right = Motor(b, PORT_C)
+    m_right.turn(-100, 15)
+
+def forward(b):
+    legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
+
+    legs[0].run(FORTH)
+    legs[1].run(FORTH)
+    sleep(1)
+    legs[0].idle()
+    legs[1].idle()
+
+def back(b):
+    legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
+    legs[0].run(BACK)
+    legs[1].run(BACK)
+    sleep(1)
+    legs[0].idle()
+    legs[1].idle()
+
+b = nxt.locator.find_one_brick()
+forward(b)
+back(b)
+left(b)
+right(b)
