481137155ccb969f47bc44cb22f9a29462ba49b3
Herrgrim0 <rgrimau@ulb.ac.be>
2019-03-22 16:16:36 +0100

    [enh] refactoring into a class

--- nxt-raspi-interface/interface.py
+++ nxt-raspi-interface/interface.py
@@ -9,51 +9,48 @@ import datetime
 FORTH = 100
 BACK = -100
 
+class Interface(object):
 
-def take_pic():
-    cam = Camera()
-    img = cam.getImage()
-    now = datetime.datetime.now()
-    path = '{}-{}-{}-{}-{}-{}'.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
-    path += ".jpg"
-    img.save(path)
-    return path
+    def __init__(self):
+        b = nxt.locator.find_one_brick()
+        cam = Camera()
+        self.path = None
 
 
-def left(b):
-    m_left = Motor(b, PORT_B)
-    m_left.turn(100, 15)
+    def take_pic():
+        img = cam.getImage()
+        now = datetime.datetime.now()
+        path = '{}-{}-{}-{}-{}-{}'.format(now.year, now.month, now.day,
+                                          now.hour, now.minute, now.second)
+        path += ".jpg"
+        img.save(path)
+        self.path = path
 
 
-def right(b):
-    m_right = Motor(b, PORT_C)
-    m_right.turn(-100, 15)
+    def left(b):
+        m_left = Motor(b, PORT_B)
+        m_left.turn(100, 15)
 
 
-def forward(b):
-    legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
-
-    legs[0].run(FORTH)
-    legs[1].run(FORTH)
-    sleep(1)
-    legs[0].idle()
-    legs[1].idle()
+    def right(b):
+        m_right = Motor(b, PORT_C)
+        m_right.turn(-100, 15)
 
 
-def back(b):
-    legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
-    legs[0].run(BACK)
-    legs[1].run(BACK)
-    sleep(1)
-    legs[0].idle()
-    legs[1].idle()
+    def forward(b):
+        legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
 
-if __name__ == '__main__':
+        legs[0].run(FORTH)
+        legs[1].run(FORTH)
+        sleep(1)
+        legs[0].idle()
+        legs[1].idle()
 
-    """b = nxt.locator.find_one_brick()
-    forward(b)
-    back(b)
-    left(b)
-    right(b)"""
-    path = take_pic()
-    print path
+
+    def back(b):
+        legs = [Motor(b, PORT_B), Motor(b, PORT_C)]
+        legs[0].run(BACK)
+        legs[1].run(BACK)
+        sleep(1)
+        legs[0].idle()
+        legs[1].idle()
