f730acf74651855476dc423e647ab90364feeb71
BeanOfChaos <benjarte@ulb.ac.be>
2019-03-22 14:56:58 +0100

    Cleaning up comments from original author + pretty prints

--- server.py
+++ server.py
@@ -5,43 +5,9 @@ from utils import SERV_PORT
 from itertools import count
 import pickle as p
 import socket
-# Get screen size so that we can initialize layers correctly based on shape
-# returned from AI gym. Typical dimensions at this point are close to 3x40x90
-# which is the result of a clamped and down-scaled render buffer in get_screen()
 
 
 episode_durations = []
-
-
-######################################################################
-# Training loop
-# ^^^^^^^^^^^^^
-#
-# Finally, the code for training our model.
-#
-# Here, you can find an ``optimize_model`` function that performs a
-# single step of the optimization. It first samples a batch, concatenates
-# all the tensors into a single one, computes :math:`Q(s_t, a_t)` and
-# :math:`V(s_{t+1}) = \max_a Q(s_{t+1}, a)`, and combines them into our
-# loss. By defition we set :math:`V(s) = 0` if :math:`s` is a terminal
-# state. We also use a target network to compute :math:`V(s_{t+1})` for
-# added stability. The target network has its weights kept frozen most of
-# the time, but is updated with the policy network's weights every so often.
-# This is usually a set number of steps but we shall use episodes for
-# simplicity.
-#
-
-######################################################################
-#
-# Below, you can find the main training loop. At the beginning we reset
-# the environment and initialize the ``state`` Tensor. Then, we sample
-# an action, execute it, observe the next screen and the reward (always
-# 1), and optimize our model once. When the episode ends (our model
-# fails), we restart the loop.
-#
-# Below, `num_episodes` is set small. You should download
-# the notebook and run lot more epsiodes, such as 300+ for meaningful
-# duration improvements.
 
 
 def get_bot_state():
@@ -79,6 +45,11 @@ while True:
     # TODO: Understand why this was done in the first place
     state = current_screen - last_screen
     for t in count():
+        print("Current length of episode: ", t)
+        print("Overall length of episode: ", end='')
+        for episode in episode_durations:
+            print(episode, end=", ")
+        print()
         # Select and perform an action
         action = rql.select_action(state)
         send_action(action)
@@ -104,17 +75,3 @@ while True:
             episode_durations.append(t + 1)
             break
         rql.target_update()
-
-
-######################################################################
-# Here is the diagram that illustrates the overall resulting data flow.
-#
-# .. figure:: /_static/img/reinforcement_learning_diagram.jpg
-#
-# Actions are chosen either randomly or based on a policy, getting the next
-# step sample from the gym environment. We record the results in the
-# replay memory and also run optimization step on every iteration.
-# Optimization picks a random batch from the replay memory to do training of the
-# new policy. "Older" target_net is also used in optimization to compute the
-# expected Q values; it is updated occasionally to keep it current.
-#
